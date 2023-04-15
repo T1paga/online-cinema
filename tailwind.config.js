f46c03d1194471ad06d2cf3aa09c4ad1ce93659e
T1paga <okru.shko@bk.ru>
2023-04-15 22:08:40 +0700

    Начальная настройка проекта, создание структуры

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/pages/**/*.{js,ts,jsx,tsx}',
+//     './src/components/**/*.{js,ts,jsx,tsx}',
+//     './src/app/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+//         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
+
+const primary = '#E30B13';
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -6,13 +28,17 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      primary,
+      black: colors.black,
+      white: colors.white,
+      transparent: colors.transparent,
+    },
+    extends: {
+      fontSize: {
+        '2lg': '1.38rem',
       },
     },
   },
   plugins: [],
-}
+};
