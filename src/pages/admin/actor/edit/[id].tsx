04838372396746ddcca42c9938eb299e8975b8fa
T1paga <okru.shko@bk.ru>
2023-04-27 20:28:36 +0700

    реализовал загрузку файлов + сделал возможность добавления и редактирования актера

---
+++ src/pages/admin/actor/edit/[id].tsx
@@ -0,0 +1,11 @@
+import ActorEdit from '@/components/pages/admin/actor/ActorEdit'
+
+import { NextPageAuth } from '@/shared/types/auth.types'
+
+const ActorEditPage: NextPageAuth = () => {
+	return <ActorEdit />
+}
+
+ActorEditPage.isOnlyAdmin = true
+
+export default ActorEditPage
